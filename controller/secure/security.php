3619e7e8e583c92662227cf61af77c9812efa566
Guilherme Pinto <guilherme.camacho@atlasproj.com>
2018-03-11 15:52:29 -0300

    Secure token mode

--- controller/secure/security.php
+++ controller/secure/security.php
@@ -55,14 +55,35 @@ class security
         }
     }
 
-
     function TokenCreate()
     {
-        return 'AEEEEEE CARALHOOO';
+        $var = [
+            'session' => session_id(),
+            'time' => date('d/m/Y'),
+            'ip' => $_SERVER['REMOTE_ADDR'],
+            'client' => $_SERVER['HTTP_USER_AGENT']
+        ];
+        file_put_contents('auth/' . session_id() . '.json', json_encode($var));
+        return session_id();
     }
 
-    function TokenValidate()
+    function TokenValidate($token)
     {
-
+        $res = false;
+        $file = 'auth/' . $token . '.json';
+        if (is_file($file)) {
+            $var = [
+                'session' => $token,
+                'time' => date('d/m/Y'),
+                'ip' => $_SERVER['REMOTE_ADDR'],
+                'client' => $_SERVER['HTTP_USER_AGENT']
+            ];
+            $atual = md5(json_encode($var));
+            $saved = md5(file_get_contents($file));
+            if ($atual == $saved) {
+                $res = true;
+            }
+        }
+        return $res;
     }
 }
